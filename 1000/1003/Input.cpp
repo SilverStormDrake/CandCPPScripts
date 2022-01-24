ebe5765eaf95368268dad77de2b727046482d2b1
André <andrelu0022@gmail.com>
2022-01-24 14:13:06 -0300

    1003 Done. Basic Input

---
+++ 1000/1003/Input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    string b;
+    /*
+    * cin >> a;
+    * cout << a << endl;
+    */
+    getline(cin, b);
+    cout << b << endl;
+
+    return 0;
+}
