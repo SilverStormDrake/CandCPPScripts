540a30f7f1ba77be97ddb2dc710fdb60d54d735e
André <andrelu0022@gmail.com>
2022-01-24 13:34:37 -0300

    First Commit

---
+++ 1000/1001/HelloWorld.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    printf("Hello World\n\n");
+
+    int x = 20;
+    string a; //Hey, this thing have strings wow
+    string b;
+
+    cout << "Nice Number: " << x << endl;
+    printf("Nice Number: %i\n", x);
+
+    scanf("%s",&a);
+    cin >> a;
+    printf("%s", a);
+    cout << b;
+
+    char c = 'A';
+    string s = "ABC";
+    int x = 3;
+    float f = 3.14159;
+    double d = 3.14159;
+    bool b = true;
+
+
+
+    return 0;
+}
